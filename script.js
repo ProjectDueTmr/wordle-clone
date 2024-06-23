ca20f0219cd915832751505317c04abdba589142
nick <nicksanchez2371@gmail.com>
2024-06-22 19:10:11 -0500

    user interaction

--- script.js
+++ script.js
@@ -15289,25 +15289,52 @@ const dictionary = [
     "rural",
     "shave"
   ]
+  const guessGrid = document.querySelector("[data-guess-grid]")
+
+  startIntercation()
 
   function startIntercation(){
     document.addEventListener("click", handleMouseClick)
     document.addEventListener("keydown", handleKeyPress)
   }
 
+  function stopInteraction(){
+    document.removeEventListener("click", handleMouseClick)
+    document.removeEventListener("keydown", handleKeyPress)
+  }
+
   function handleMouseClick(){
-    if(e.target,matches("data-key")) {
+    if(e.target,matches("[data-key]")) {
         pressKey(e.target.dataset.key)
         return
     }
 
-    if(e.target.matches("[data-enter")) {
+    if(e.target.matches("[data-enter]")) {
         submitGuess()
         return 
+    }
+    if(e.target.matches('[data-delete]')){
+      deleteKey()
+      return
     }
 }
   
 
   function handleKeyPress(e) {
+    if(e.key === "Enter"){
+      submitGuess()
+      return 
+    }
+    if(e.key === "Backspace" || e.key === "Delete"){
+      deleteKey()
+      return
+    }
+    if(e.key.match(/^[a-z]$/)){
+      pressKey(e.key)
+      return
+    }
+  }
+
+  function pressKey(key){
 
   }
